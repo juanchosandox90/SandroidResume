fe43bdfc2fa3755df69984bb1ea362ea265c66ca
jsandoval <jsandoval@wearenovae.com>
2018-10-03 15:56:28 -0500

    Finished First Configurations
    1. Splash Screen Finished
    2. Icon Finished
    3. General Background Finished
    4. Particles Implementation

---
+++ app/src/main/java/jcsandoval/sandroidresume/com/sandroidresume/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package jcsandoval.sandroidresume.com.sandroidresume;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
+
+    }
+}
