71c043258c7bcc2895ff262c1912a66330d14877
jsandoval <jsandoval@wearenovae.com>
2018-10-03 12:20:26 -0500

    First Commit to dev 1. Add dependency for particles background customization 2. Add colors for the app 3. Change name of the app

--- app/src/main/java/jcsandoval/sandroidresume/com/sandroidresume/MainActivity.java
+++ app/src/main/java/jcsandoval/sandroidresume/com/sandroidresume/MainActivity.java
@@ -3,11 +3,29 @@ package jcsandoval.sandroidresume.com.sandroidresume;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.doctoror.particlesdrawable.ParticlesDrawable;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private final ParticlesDrawable mDrawable = new ParticlesDrawable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.view).setBackground(mDrawable);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDrawable.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mDrawable.stop();
     }
 }
